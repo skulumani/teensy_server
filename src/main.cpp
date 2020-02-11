840dd0766eb2af9c308133bbc7c0a84d414cd3ae
Shankar Kulumani <shanks.k@gmail.com>
2020-02-10 20:25:55 -0500

    Setup imu protobuf message

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,23 @@
 #include <Eigen/Dense>
 
 #include "serial_port.hpp"
-#include "proto/simple.pb.h"
+#include "simple.pb.h"
+#include "ahrs.pb.h"
 
 int main () {
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
     SimpleMessage m;
     SerialPort serial("/dev/cu.usbmodem5105671", 115200);
 
-    serial.list_ports();
-
+    /* serial.list_ports(); */
+    std::cout << "Waiting 2 seconds" << std::endl;
+    usleep(2000);
     while(true) {
         serial.read();
         /* usleep(1000); */
     }
     return 0;
+
+    google::protobuf::ShutdownProtobufLibrary();
 }
