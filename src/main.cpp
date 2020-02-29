c8964ce3f19f92132bb47099259e8bc1af76deb5
Shankar Kulumani <shanks.k@gmail.com>
2020-02-28 22:00:27 -0500

    Reading messages and printing to screen

--- src/main.cpp
+++ src/main.cpp
@@ -20,17 +20,22 @@ int main () {
     unsigned long t1 = 0;
     unsigned long t2 = 0;
     unsigned long dt = 0;
-
+    
+    int c = 0;
+    std::cout << "Press any key to exit" << std::endl;
     while(true) {
         serial.read();
         /* m.ParseFromArray(serial.byte_buffer, sizeof(serial.byte_buffer)); */
         /* std::cout << m.lucky_number() << std::endl; */
         imu_msg.ParseFromArray(serial.byte_buffer, sizeof(serial.byte_buffer));
         t1 = imu_msg.time();
-        std::cout << imu_msg.time() << " " << imu_msg.ax() << std::endl;
-        /* usleep(1000); */
+        std::cout << "\33[2K\r" << "T: " << imu_msg.time() << " " 
+            << imu_msg.accel().x() << " " << imu_msg.accel().y() << " " << imu_msg.accel().z() 
+            << " " << imu_msg.gyro().x() << " " << imu_msg.gyro().y() << " " << imu_msg.gyro().z() 
+            << " " << imu_msg.mag().x() << " " << imu_msg.mag().y() << " " << imu_msg.mag().z() 
+            << " " << imu_msg.temp() << std::flush;
     }
-    return 0;
 
     google::protobuf::ShutdownProtobufLibrary();
+    return 0;
 }
