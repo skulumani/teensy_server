35f4a7e6651fbb3b8dcd1cc6bb67da8ad1258a9c
Shankar Kulumani <shanks.k@gmail.com>
2020-03-07 18:32:36 -0500

    Write todo

--- src/calibrate_imu.cpp
+++ src/calibrate_imu.cpp
@@ -10,5 +10,11 @@ int main () {
 
     // create a HDF5 to store the sensor data
     /* HDF5::File hf = HDF5::File("imu_data.hdf5", HDF5::Truncate); */
+
+    // read HDF5 with all the IMU data
+    // parse all the magnetometer data
+    // call ellipsoid fit
+    // compute the Mag scale parameters
+    // print and write to HDF5
     return 0;
 }
