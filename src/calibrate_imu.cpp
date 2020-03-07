f4507d4d4e7b74878f5d6759442c1ef4e5db4348
Shankar Kulumani <shanks.k@gmail.com>
2020-03-07 18:31:35 -0500

    Start on calibration driver function

---
+++ src/calibrate_imu.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+#include <Eigen/Dense>
+
+#include "ahrs.hpp"
+#include "hdf5.hpp"
+
+int main () {
+
+    // create a HDF5 to store the sensor data
+    /* HDF5::File hf = HDF5::File("imu_data.hdf5", HDF5::Truncate); */
+    return 0;
+}
