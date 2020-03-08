a157f2eb311f291a82a59ce8437248877f859422
Shankar Kulumani <shanks.k@gmail.com>
2020-03-08 17:52:54 -0400

    Update imu reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <chrono>
 
 #include <Eigen/Dense>
 
@@ -12,7 +13,7 @@ int main () {
     GOOGLE_PROTOBUF_VERIFY_VERSION;
 
     // create a HDF5 to store the sensor data
-    HDF5::File hf = HDF5::File("imu_data.hdf5", HDF5::File::Truncate );
+    HDF5::File hf = HDF5::File("gyro_data.hdf5", HDF5::File::Truncate );
     Eigen::Matrix<float, Eigen::Dynamic, 11> imu_data;
     Eigen::Matrix<float, 1, 11> imu_row;
     
@@ -32,15 +33,15 @@ int main () {
     unsigned long t1 = 0;
     unsigned long t2 = 0;
     unsigned long dt = 0;
-    
-    int c = 0;
-    std::cout << "Press any key to exit" << std::endl;
-    while(true) {
+   
+    auto start_time = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = std::chrono::system_clock::now() - start_time;
+
+    while(elapsed_seconds.count() < 60.0) {
         serial.read();
         /* m.ParseFromArray(serial.byte_buffer, sizeof(serial.byte_buffer)); */
         /* std::cout << m.lucky_number() << std::endl; */
         imu_msg.ParseFromArray(serial.byte_buffer, sizeof(serial.byte_buffer));
-        t1 = imu_msg.time();
         std::cout << "\33[2K\r" << "T: " << imu_msg.time() << " " 
             << imu_msg.accel().x() << " " << imu_msg.accel().y() << " " << imu_msg.accel().z() 
             << " " << imu_msg.gyro().x() << " " << imu_msg.gyro().y() << " " << imu_msg.gyro().z() 
@@ -56,9 +57,13 @@ int main () {
         imu_data.conservativeResize(num_meas+1, 11);
         imu_data.row(num_meas) << imu_row;
         num_meas += 1;
+        
+        elapsed_seconds = std::chrono::system_clock::now() - start_time;
 
     }
     
+    std::cout << std::endl << std::endl;
+
     // save to HDF5 file
     hf.write("imu_data", imu_data);
 
